0858fee5bf2e6842236c39852bc0385a91008c96
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-02-25 19:05:56 -0600

    ready to push it

--- sql/connection.js
+++ sql/connection.js
@@ -7,9 +7,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '35.188.48.136',
         user: 'root',
-        password: 'password',
+        password: 'jfjfjf',
         database: 'admin'
       }
 
